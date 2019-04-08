4893d4094360ef5e64eb8ed41a6957b29887d4af
sols000 <406007603@qq.com>
2019-04-09 00:05:23 +0800

    Delete the exe

--- StudyDemo2.cpp
+++ StudyDemo2.cpp
@@ -9,6 +9,9 @@ int main()
     cout<<"This is the second line"<<endl;
     cout<<"This is the third line"<<endl;
     cout<<"This is the Fourth line"<<endl;
+    int a = 0;
+    a++;
+    cout <<"Number is:"<<a<<endl;
     getchar();
     return 0;
 
